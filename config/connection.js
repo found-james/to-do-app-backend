7338744f253c38e1408ec7f72fad88ea3061deaf
James Suescun <james.steven2318@gmail.com>
2022-04-19 12:00:47 -0400

    still just trying stuff

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 require("dotenv").config()
 
-const connectDB = async () => {
+const connectDB = () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
+        const conn = mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
           });
